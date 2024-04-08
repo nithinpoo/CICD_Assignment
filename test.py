8ec74cefe54ac788c43157b82e8a4053dc392cd8
nithinpoo <88471849+nithinpoo@users.noreply.github.com>
2024-04-08 11:54:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier  
 import pickle
 import numpy as np
 
-df = pd.read_csv("data/test.csv")
+df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-with open("model.pkl", 'rb') as f:
-    model = pickle.load(f)
+# Use RandomForestClassifier instead of LogisticRegression
+model = RandomForestClassifier().fit(X, y)
 
-print(round(model.score(X, y), 3))
+with open("model.pkl", 'wb') as f:
+
+    pickle.dump(model, f)
